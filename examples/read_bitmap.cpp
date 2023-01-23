7d53dda3beb6bf1ada5c4a4ed7cdf55b6f0b6147
Bader <badereddineouaich@gmail.com>
2023-01-23 23:17:58 +0100

    updated read_bitmap example

---
+++ examples/read_bitmap.cpp
@@ -0,0 +1,31 @@
+#include "BitmapPlusPlus.hpp"
+
+int main(void)
+{
+	try
+	{
+
+		bmp::Bitmap image;
+
+		// Load penguin.bmp bitmap
+		image.load("penguin.bmp");
+
+		// Modify loaded image (makes half of the image black)
+		for (std::int32_t y = 0; y < image.height(); ++y)
+		{
+			for (std::int32_t x = 0; x < image.width() / 2; ++x)
+			{
+				image.set(x, y, bmp::Black);
+			}
+		}
+
+		// Save
+		image.save("modified-penguin.bmp");
+
+		return EXIT_SUCCESS;
+	}
+	catch (const bmp::Exception &e)
+	{
+		return EXIT_FAILURE;
+	}
+}

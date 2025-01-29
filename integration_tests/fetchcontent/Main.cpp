35cc2cc9a53afe5c411f6005a1505fb2742ba078
baderouaich <badereddineouaich@gmail.com>
2025-01-29 13:00:46 +0100

    Added FetchContent integration example

---
+++ integration_tests/fetchcontent/Main.cpp
@@ -0,0 +1,12 @@
+#include <BitmapPlusPlus.hpp>
+
+int main()
+{
+    bmp::Bitmap bitmap(512, 512);
+    for (auto& pixel : bitmap)
+    {
+        pixel = bmp::Pixel(42, 69, 96);
+    }
+
+    bitmap.save("demo.bmp");
+}
